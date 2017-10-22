7fae135f562dbdbb0dc9a6a45a68f34e9bdd50ba
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-21 23:49:11 -0500

    base mode almost complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,17 +8,23 @@ $('form').on('submit', function (event) {
     event.preventDefault();
     submitTask();
 });
-
+$('#seeToDos').on('click', '#deleteButton', deleteClicked);
+$('#seeToDos').on('click', '#completeButton', completeClicked);
 }
-
-function submitTask() {
     var toDoItem = $('#toDoIn').val();
     var noteText = $('#noteIn').val();
+    var todoComplete = false;
+
+function submitTask() {
+    // var toDoItem = $('#toDoIn').val();
+    // var noteText = $('#noteIn').val();
+    // var todoComplete = false;
     console.log('task:', toDoItem);
     console.log('note:', noteText);
     var objectToSend = {
         task: toDoItem,
-        notes: noteText
+        notes: noteText,
+        complete: complete
     }
     $('#toDoIn').val('');
     $('#noteIn').val('');
@@ -55,14 +61,57 @@ function appendToDos(todos) {
     $('#seeToDos').empty();
     for (var i = 0; i < todos.length; i += 1) {
         var todo = todos[i];
-        var $tr = $('<tr></tr>');
+        var $tr = $('<p></p>');
         $tr.data('To Do:', todo);
-        $tr.append('<td>' + todo.task + '</td>');
-        $tr.append('<td>' + todo.notes + '</td>');
-        $tr.append('<td><button class="deleteButton" class="btn btn-danger" data-id="' + todo.id + '">Delete</button></td>');
-
-        
+        $tr.append('<span>To do: ' + todo.task +  '  </span> ');
+        $tr.append('<span> Notes: ' + todo.notes +  '  </span> ');
+        $tr.append('<button id="deleteButton" class="btn btn-danger" data-id="' + todo.id + '">Delete</button>');
+        $tr.append('<button id="completeButton" class="btn btn-success" data-id="' + todo.id + '">Complete</button><br/>');
         $('#seeToDos').append($tr);
     }
 }
+
+function deleteClicked() {
+    var todoId = $(this).data('id');
+    console.log('Delete todo with id', todoId);
+    $.ajax({
+        method: 'DELETE', 
+        url: '/todo/' + todoId,
+    }).done(function (response){
+        refreshToDos();
+    }).fail(function (error){
+        console.log('something went wrong', error);
+    })
+}
+
+function completeClicked() {
+    var todoId = $(this).data('id');
+    $.ajax({
+        method: 'PUT', 
+        url: '/todo/' + todoId,
+    }).done(function(response){
+        console.log(response);
+        refreshToDos();
+        changeCompleted(todoId);
+    }).fail(function(error){
+        console.log('Did not complete correctly', error);
+    });
+}
+
+    function changeCompleted(todoId) {
+        console.log(todoId);
+        
+        
+        // if ( todoComplete === true) {
+        //     console.log('true');
+        // }
+        // else {
+        //     console.log('false');
+        }
+        
+
+    
+ 
+
+
 
--- server/routers/todo_routes.js
+++ server/routers/todo_routes.js
@@ -62,3 +62,51 @@ router.get('/', function (req, res) {
 
 }); //END GET ROUTE
 module.exports = router;
+
+router.delete('/:id', function (req, res) {
+    var todoId = req.params.id;
+    console.log(todoId);
+    pool.connect(function (errorConnectingToDb, db, done) {
+
+        if (errorConnectingToDb) {
+            console.log('Error connecting', errorConnectingToDb);
+            res.send(500);
+        } else {
+            var queryText = 'DELETE FROM "todo_list" WHERE "id" = $1;';
+            db.query(queryText, [todoId], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery)
+                    res.send(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            }) //END QUERY
+        }
+    }); //END POOL
+
+}); //END delete ROUTE
+
+router.put('/:id', function (req, res) {
+    var todoId = req.params.id;
+    console.log(todoId);
+    pool.connect(function (errorConnectingToDb, db, done) {
+
+        if (errorConnectingToDb) {
+            console.log('Error connecting', errorConnectingToDb);
+            res.send(500);
+        } else {
+            var queryText = 'UPDATE "todo_list" SET "complete" = $1 WHERE "id" = $2;';
+            db.query(queryText, ['true', todoId], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery)
+                    res.send(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            }) //END QUERY
+        }
+    }); //END POOL
+
+}); //END PUT ROUTE
